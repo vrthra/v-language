d7666123ed69f340f48e6936b4bc89a5efe426e2
blufox <blufox@gmail.com>
2007-03-09 10:09:04 +0000

    wrapping up stack. M    v/QStack.java

--- v/QStack.java
+++ v/QStack.java
@@ -4,6 +4,21 @@ import java.util.*;
 public class QStack extends Stack<Term> {
     public QStack() {
         super();
-        // just a wrapper for debugging purposes.
+    }
+
+    public Term push(Term t) {
+        return super.push(t);
+    }
+
+    public Term pop() {
+        return super.pop();
+    }
+
+    public boolean empty() {
+        return super.empty();
+    }
+
+    public Term peek() {
+        return super.peek();
     }
 }
