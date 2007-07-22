d0861a0e2158367821593ff9c55b3c864a6a7af0
blufox <blufox@gmail.com>
2007-07-22 13:44:27 +0000

    more primitive functions. M    csrc/prologue.cpp

--- csrc/prologue.cpp
+++ csrc/prologue.cpp
@@ -76,7 +76,7 @@ void evaltmpl(TokenStream* tmpl, TokenStream* elem, SymbolMap& symbols) {
                                     if (!strcmp(tmplterm->value(),lastelem->value()))
                                         break;
                                     else
-                                        throw VException("err:evaltmpl:eq ",tmplterm->value());
+                                        throw VException("err:view:eq", "%s != %s",tmplterm->value(), lastelem->value());
                             }
 
                         } else {
@@ -93,9 +93,9 @@ void evaltmpl(TokenStream* tmpl, TokenStream* elem, SymbolMap& symbols) {
                     }
                     break;
                 } catch (VException& e) {
-                    throw VException(e);
+                    throw e;
                 } catch (...) {
-                    throw VException("err:evaltmpl:sym ",t->value());
+                    throw VException("err:view:sym",t->value());
                 }
 
             case TQuote:
@@ -104,9 +104,9 @@ void evaltmpl(TokenStream* tmpl, TokenStream* elem, SymbolMap& symbols) {
                     Token* et = estream->next();
                     evaltmpl(t->qvalue()->tokens(), et->qvalue()->tokens(), symbols);
                 } catch (VException& e) {
-                    throw VException(e);
+                    throw e;
                 } catch (...) {
-                    throw VException("err:evaltmpl:quote ",t->value());
+                    throw VException("err:view:quote",t->value());
                 }
                 break;
             default:
@@ -115,7 +115,7 @@ void evaltmpl(TokenStream* tmpl, TokenStream* elem, SymbolMap& symbols) {
                 if (!strcmp(t->value(),eterm->value()))
                     break;
                 else
-                    throw VException("err:evaltmpl:eq " ,t->value());
+                    throw VException("err:view:eq.1", "%s != %s" ,t->value(), eterm->value());
         }
     }
 
@@ -164,6 +164,20 @@ TokenStream* evalres(TokenStream* res, SymbolMap& symbols) {
 
 }
 
+struct Ctrue : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        p->push(new Term(TBool, true));
+    }
+};
+
+struct Cfalse : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        p->push(new Term(TBool, false));
+    }
+};
+
 struct Cadd : public Cmd {
     void eval(VFrame* q) {
         VStack* p = q->stack();
@@ -183,6 +197,14 @@ struct Cputs : public Cmd {
         VStack* p = q->stack();
         Token* a = p->pop();
         V::outln(a->value());
+    }
+};
+
+struct Cput : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* a = p->pop();
+        V::out(a->value());
     }
 };
 
@@ -245,6 +267,32 @@ struct Cdef : public Cmd {
     }
 };
 
+struct Cdefenv : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* b = p->pop();
+        Token* t = p->pop();
+        SymPair entry = splitdef(t->qvalue());
+        char* symbol = entry.first;
+        b->fvalue()->def(symbol, entry.second);
+    }
+};
+
+struct Cparent : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        VFrame* t = p->pop()->fvalue();
+        p->push(new Term(TFrame, t->parent()));
+    }
+};
+
+struct Cme : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        p->push(new Term(TFrame, q->parent()));
+    }
+};
+
 struct Cuse : public Cmd {
     void eval(VFrame* q) {
         VStack* p = q->stack();
@@ -259,17 +307,187 @@ struct Cuse : public Cmd {
             CmdQuote* module = new CmdQuote(new LexStream(cs));
             module->eval(q->parent());
         } catch (...) {
-            throw new VException("err:use", file->svalue());
+            throw new VException("err:use", file->value());
         }
     }
 };
 
+struct Cuseenv : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* env = p->pop();
+        Token* file = p->pop();
+        try {
+            char* v = file->svalue();
+            int len = strlen(v);
+            char* val = new char[len + 3];
+            std::sprintf(val,"%s%s",v,".v");
+
+            FileCharStream* cs = new FileCharStream(val);
+            CmdQuote* module = new CmdQuote(new LexStream(cs));
+            module->eval(env->fvalue());
+        } catch (...) {
+            throw new VException("err:*use", "%s %s",env->value(), file->value());
+        }
+    }
+};
+
+struct Ceval : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* str = p->pop();
+        try {
+            char* v = str->svalue();
+
+            BuffCharStream* cs = new BuffCharStream(v);
+            CmdQuote* module = new CmdQuote(new LexStream(cs));
+            module->eval(q->parent());
+        } catch (...) {
+            throw new VException("err:eval", str->value());
+        }
+    }
+};
+
+struct Cevalenv : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* env = p->pop();
+        Token* str = p->pop();
+        try {
+            char* v = str->svalue();
+
+            BuffCharStream* cs = new BuffCharStream(v);
+            CmdQuote* module = new CmdQuote(new LexStream(cs));
+            module->eval(env->fvalue());
+        } catch (...) {
+            throw new VException("err:*eval", "%s %s", env->value(), str->value());
+        }
+    }
+};
+
+struct Cdequote : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* prog = p->pop();
+        prog->qvalue()->eval(q->parent());
+    }
+};
+
+struct Cdequoteenv : public Cmd {
+    void eval(VFrame* q) {
+        VStack* p = q->stack();
+        Token* prog = p->pop();
+        Token* env = p->pop();
+        prog->qvalue()->eval(env->fvalue());
+    }
+};
 
 void Prologue::init(VFrame* frame) {
-    frame->def("+", new Cadd());
-    frame->def("puts", new Cputs());
-    frame->def("??", new Cshow());
-    frame->def("view", new Cview());
     frame->def(".", new Cdef());
+    frame->def("&.", new Cdefenv());
+    frame->def("&parent", new Cparent());
+    frame->def("$me", new Cme());
+
+    frame->def("puts", new Cputs());
+    frame->def("put", new Cputs());
+
+    frame->def("i", new Cdequote());
+    frame->def("&i", new Cdequote());
+
+    frame->def("view", new Cview());
     frame->def("use", new Cuse());
+    frame->def("&use", new Cuseenv());
+    frame->def("eval", new Ceval());
+    frame->def("&eval", new Cevalenv());
+
+    frame->def("true", new Ctrue());
+    frame->def("false", new Cfalse());
+
+    frame->def("+", new Cadd());
+    
+    frame->def("??", new Cshow());
+/*
+        iframe.def("module", _defmodule);
+        iframe.def("&words", _words);
+
+        iframe.def("trans", _trans);
+        iframe.def("java", _java);
+
+        iframe.def("shield", _shield);
+        iframe.def("throw", _throw);
+        iframe.def("stack", _stack);
+        iframe.def("unstack", _unstack);
+
+        iframe.def("and", _and);
+        iframe.def("or", _or);
+        iframe.def("not", _not);
+
+        //control structures
+        iframe.def("ifte", _ifte);
+        iframe.def("if", _if);
+        iframe.def("while", _while);
+        iframe.def("choice", _choice);
+
+        //others
+        iframe.def("?", _peek);
+        iframe.def("?debug", _vdebug);
+        iframe.def("?stack", _show);
+        iframe.def("?frame", _dframe);
+        iframe.def("debug", _debug);
+
+        iframe.def("abort", _abort);
+
+        //list
+        iframe.def("size", _size);
+        iframe.def("in?", _isin);
+        iframe.def("at", _at);
+        iframe.def("drop", _drop);
+        iframe.def("take", _take);
+
+
+        // on list
+        iframe.def("step", _step);
+        iframe.def("map!", _map);
+        iframe.def("map", _map_i);
+        iframe.def("filter!", _filter);
+        iframe.def("filter", _filter_i);
+        iframe.def("split!", _split);
+        iframe.def("split", _split_i);
+        iframe.def("fold!", _fold);
+        iframe.def("fold", _fold_i);
+
+        //arith
+        iframe.def("+", _add);
+        iframe.def("-", _sub);
+        iframe.def("*", _mul);
+        iframe.def("/", _div);
+
+        //bool
+        iframe.def("=", _eq);
+        iframe.def("==", _eq);
+        iframe.def("!=", _neq);
+        iframe.def(">", _gt);
+        iframe.def("<", _lt);
+        iframe.def("<=", _lteq);
+        iframe.def(">=", _gteq);
+
+        //predicates
+        iframe.def("integer?", _isinteger);
+        iframe.def("double?", _isdouble);
+        iframe.def("boolean?", _isbool);
+        iframe.def("symbol?", _issym);
+        iframe.def("list?", _islist);
+        iframe.def("char?", _ischar);
+        iframe.def("number?", _isnum);
+        iframe.def("string?", _isstr);
+
+        iframe.def(">string", _tostring);
+        iframe.def(">int", _toint);
+        iframe.def(">decimal", _todecimal);
+
+        iframe.def("help", _help);
+
+        Quote libs = Util.getdef("'std' use");
+        libs.eval(iframe);
+ */
 }
