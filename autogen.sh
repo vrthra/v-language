881d8723bd3e04e580ddbf38848f4dfd585cc59c
blufox <blufox@gmail.com>
2007-12-28 15:50:55 +0000

    Remove unneeded files. D    configure D    Makefile.in D    depcomp D    config.guess D    csrc/Makefile.in D    config.sub D    ltmain.sh D    config.h.in D    missing M    autogen.sh D    aclocal.m4 D    install-sh

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,23 @@
-touch AUTHORS COPYING NEWS README ChangeLog
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-autoreconf -fvi
+#!/bin/sh
+
+srcdir="`dirname $0`"
+test -z "$srcdir" && srcdir=.
+
+#export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+libtoolize --copy --force
+aclocal
+autoheader
+automake -a -c
+autoconf
+#autoreconf -fvi
+
+if [ "$1" = "--noconfigure" ]; then 
+    exit 0;
+fi
+
+if [ X"$@" = X  -a "X`uname -s`" = "XSunOS" ]; then
+    CC=/opt/SUNWspro/bin/cc CXX=/opt/SUNWspro/bin/CC $srcdir/configure "$@"
+else
+    $srcdir/configure "$@"
+fi
 
