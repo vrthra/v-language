f4733268d62d891f69a8066d30861a41370e20ee
blufox <blufox@gmail.com>
2007-12-31 04:01:50 +0000

    closure demonstration.
    A    closure.v

---
+++ scripts/closure.v
@@ -0,0 +1,16 @@
+[a 1].
+[b 1].  
+[closure
+    [a 100].
+    [b 200].
+    $me
+].
+
+# generate a closure and evaluate the value of a in the closure
+closure [a] &i
+
+# duplicate a closure and rebind the value of a in it,
+# then check that the rebound value is present in the original
+# closure
+closure dup [[a 101].] &i [a] &i
+
